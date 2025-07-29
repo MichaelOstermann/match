828bda093618b10345f0f8c5ad15378da1ad8d48
Michael Ostermann <michaelostermann@me.com>
2025-07-29 12:46:55 +0200

    feat(match): improve matching of multiple union fields

--- packages/match/src/internals/shape.ts
+++ packages/match/src/internals/shape.ts
@@ -1,4 +1,5 @@
-import type { ConditionalPick, OptionalKeysOf, Primitive, RequireAtLeastOne, RequiredKeysOf, Simplify } from "type-fest"
+import type { ConditionalPick, OptionalKeysOf, Primitive, RequireAtLeastOne, RequiredKeysOf, Simplify, UnionToTuple } from "type-fest"
+import type { IsUnion } from "type-fest/source/internal"
 import type { IfTrue, IsLiteral, IsTrue } from "./helpers"
 
 type IsPlainObject<T> = T extends object
@@ -30,18 +31,42 @@ export type DropShape<T, U> = T extends unknown
     ? IsPlainObject<T> extends true
         ? [MatchShape<T, U>] extends [never]
                 ? T
-                : Simplify<CompleteShape<T & {
-                    [K in keyof U]: K extends keyof T
-                        ? U[K] extends T[K]
-                            // We can only safely remove singleton sets.
-                            ? IsLiteral<U[K]> extends true
-                                ? Exclude<T[K], U[K]>
-                                : T[K]
-                            : never
-                        : never
-                }>>
+                : DropLiteralFields<T, U>
         : T
     : never
+
+// DropLiteralFields<{ foo: boolean, bar: boolean }, { foo: true, bar: false }>
+// { foo: false, bar: true }
+type DropLiteralFields<T, U> = HasLiteralFields<U> extends true
+    ? LiteralFieldsOf<U> extends infer V
+        ? Exclude<ShapeVariations<T, V>, V>
+        : never
+    : T
+
+// ShapeVariations<{ foo: boolean, bar: boolean }, { foo: true, bar: false }>
+// { foo: true, bar: true } | { foo: false, bar: false } | { foo: true, bar: false } | { foo: false, bar: true }
+type ShapeVariations<T, U, V = UnionKeysOf<T, U>> = V extends [infer Key, ...infer Rest]
+    ? Key extends keyof T
+        ? ShapeVariations<FieldVariations<T, Key>, U, Rest>
+        : never
+    : T
+
+// FieldVariations<{ foo: boolean, bar: boolean }, "foo">
+// { foo: true, bar: boolean } | { foo: false, bar: boolean }
+type FieldVariations<T, K extends keyof T, V = UnionToTuple<T[K]>> = V extends [infer Value, ...infer Rest]
+    ? T & Record<K, Value> | FieldVariations<T, K, Rest>
+    : never
+
+// Takes two records T and U, returns shared keys where T[K] is a union and U[K] is a literal.
+type UnionKeysOf<T, U> = UnionToTuple<keyof {
+    [K in keyof U]: K extends keyof T
+        ? IsLiteral<U[K]> extends true
+            ? IsUnion<T[K]> extends true
+                ? K
+                : never
+            : never
+        : never
+}>
 
 // Used to retrieve all the possible leftover shapes that can be matched.
 export type ShapePatterns<T> = T extends unknown
@@ -83,13 +108,6 @@ type MatchShape<T, U> = IfTrue<{
         : false
 }[keyof U], T, never>
 
-// Returns T, unless any field was never.
-type CompleteShape<T> = IfTrue<{
-    [K in keyof T]: [T[K]] extends [never]
-        ? true
-        : false
-}[keyof T], never, T>
-
 // Expands { foo?: boolean } into { foo: boolean | undefined }.
 // Depending on tsconfig, { foo: undefined } might not be assignable to { foo?: boolean }
 // otherwise, making it impossible to pattern match optional fields.
@@ -99,9 +117,11 @@ type CleanOptionals<T extends object> = Simplify<{
     [K in OptionalKeysOf<T>]: T[K] | undefined
 }>
 
-type AtLeastOneField<T> = RequireAtLeastOne<T, keyof T>
+type LiteralFieldsOf<T> = {
+    [K in keyof T as IsLiteral<T[K]> extends true ? K : never]: T[K]
+}
 
 // Checks whether the given object as any field containing a literal.
-type HasLiteralFields<T> = IsTrue<{
-    [K in keyof T]: IsLiteral<T[K]>
-}[keyof T]>
+type HasLiteralFields<T> = [keyof LiteralFieldsOf<T>] extends [never] ? false : true
+
+type AtLeastOneField<T> = RequireAtLeastOne<T, keyof T>
